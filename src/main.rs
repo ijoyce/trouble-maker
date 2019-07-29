b0495f28b576922f8c17d1065c034f060d13ee9d
Ian Joyce <ian.joyce@gmail.com>
2019-07-28 20:44:34 -0500

    starting over.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-#![deny(warnings)]
+//#![deny(warnings)]
 extern crate hyper;
 extern crate pretty_env_logger;
 
-use hyper::rt::{self, Future};
+use futures::{future, Future};
 use hyper::service::service_fn;
-use hyper::{Client, Server};
-use std::net::SocketAddr;
+use hyper::{Body, Request, Response, Server, StatusCode};
+
+use std::thread;
+use std::time::Duration;
+
+type GenericError = Box<dyn std::error::Error + Send + Sync>;
+type ResponseFuture = Box<dyn Future<Item = Response<Body>, Error = GenericError> + Send>;
 
 #[derive(Debug)]
 enum Failure {
@@ -19,11 +24,13 @@ struct Path {
     path: String,
     failure: Failure,
     frequency: f32,
+    delay: u64,
 }
 
 #[derive(Debug)]
 struct Configuration {
     paths: Vec<Path>,
+    listen_address: String,
 }
 
 impl Configuration {
@@ -34,66 +41,85 @@ impl Configuration {
     }
 }
 
-fn init_config() -> Configuration {
+fn init() -> Configuration {
     // TODO: Read from file.
     Configuration {
+        listen_address: String::from("127.0.0.1:3001"),
         paths: vec![
             Path {
                 path: "/error".to_string(),
                 failure: Failure::Error,
                 frequency: 0.5,
+                delay: 300,
             },
             Path {
                 path: "/delay".to_string(),
                 failure: Failure::Delay,
                 frequency: 0.25,
+                delay: 800,
             },
             Path {
                 path: "/timeout".to_string(),
                 failure: Failure::Timeout,
                 frequency: 0.4,
+                delay: 300,
             },
         ],
     }
 }
 
+fn new_service(req: Request<Body>) -> ResponseFuture {
+    let config = init();
+
+    // Apply failure.
+    for p in &config.paths {
+        if p.path == req.uri().path() {
+            match p.failure {
+                Failure::Error => {
+                    println!("{:?}", Failure::Error);
+                    thread::sleep(Duration::from_millis(p.delay));
+                }
+                Failure::Delay => {
+                    println!("{:?}", Failure::Delay);
+                    let x: f32 = rand::random();
+                    if x <= p.frequency {
+                        thread::sleep(Duration::from_millis(p.delay));
+                    }
+                }
+                Failure::Timeout => {
+                    println!("{:?}", Failure::Timeout);
+                    thread::sleep(Duration::from_millis(p.delay));
+                }
+            }
+        }
+    }
+
+    let body = Body::from("Not Found");
+    Box::new(future::ok(
+        Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(body)
+            .unwrap(),
+    ))
+}
+
 fn main() {
     pretty_env_logger::init();
 
-    let in_addr = ([127, 0, 0, 1], 3001).into();
-    let out_addr: SocketAddr = ([66, 39, 158, 129], 80).into();
-
-    let config = init_config();
+    let config = init();
     config.print();
 
-    let client_main = Client::new();
+    let addr = config.listen_address.parse().unwrap();
 
-    // new_service is run for each connection, creating a 'service'
-    // to handle requests for that specific connection.
-    let new_service = move || {
-        let client = client_main.clone();
-        // This is the `Service` that will handle the connection.
-        // `service_fn_ok` is a helper to convert a function that
-        // returns a Response into a `Service`.
-        service_fn(move |mut req| {
-            let uri_string = format!(
-                "http://{}{}",
-                out_addr,
-                req.uri().path_and_query().map(|x| x.as_str()).unwrap_or("")
-            );
-            let uri = uri_string.parse().unwrap();
-            *req.uri_mut() = uri;
-            println!(" -> {}", req.uri());
-            client.request(req)
-        })
-    };
+    hyper::rt::run(future::lazy(move || {
+        let new_service = move || service_fn(move |req| new_service(req));
 
-    let server = Server::bind(&in_addr)
-        .serve(new_service)
-        .map_err(|e| eprintln!("server error: {}", e));
+        let server = Server::bind(&addr)
+            .serve(new_service)
+            .map_err(|e| eprintln!("server error: {}", e));
 
-    println!("Listening on http://{}", in_addr);
-    println!("Proxying on http://{}", out_addr);
+        println!("Listening on http://{}", addr);
 
-    rt::run(server);
+        server
+    }));
 }
