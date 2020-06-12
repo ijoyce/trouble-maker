449ae24cc376ed19177b8022aedff1f5444b5a88
Ian Joyce <ian.joyce@gmail.com>
2020-06-12 13:49:55 -0500

    Move config bits to config.rs.

--- src/main.rs
+++ src/main.rs
@@ -8,55 +8,15 @@ use http::Uri;
 use hyper::service::service_fn;
 use hyper::{Body, Client, Request, Response, Server, StatusCode};
 use regex::Regex;
-use serde::{Deserialize, Serialize};
 
 use std::thread;
 use std::time::{Duration, Instant};
 
+mod config;
+use crate::config::*;
+
 type GenericError = Box<dyn std::error::Error + Send + Sync>;
 type ResponseFuture = Box<dyn Future<Item = Response<Body>, Error = GenericError> + Send>;
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
-enum FailureType {
-    Error,
-    Delay,
-    Timeout,
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
-struct Failure {
-    path: String,
-    failure_type: FailureType,
-    frequency: f32,
-    delay: u64,
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
-struct Configuration {
-    failures: Vec<Failure>,
-    listener_address: String,
-    proxy_address: String,
-}
-
-impl Configuration {
-    fn print(&self) {
-        for f in &self.failures {
-            info!("{:?}", f);
-        }
-    }
-}
-
-fn init() -> Configuration {
-    let mut config = config::Config::default();
-    config
-        .merge(config::File::with_name("Configuration"))
-        .unwrap();
-    let config = config.try_into::<Configuration>().unwrap();
-
-    info!("Settings: {:?}", config);
-
-    config
-}
 
 fn new_service(req: Request<Body>, config: &Configuration) -> ResponseFuture {
     // Apply failure.
@@ -165,7 +125,7 @@ fn main() {
     hyper::rt::run(future::lazy(move || {
         let now = Instant::now();
 
-        let config = init();
+        let config = config::init();
         config.print();
 
         let listening_addr = config.listener_address.parse().unwrap();
