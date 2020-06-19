d69008e333f6fd3ea178d1e47664e24a23ae3e44
Ian Joyce <ian.joyce@gmail.com>
2020-06-19 14:07:12 -0500

    Move config bits to it's own file.

---
+++ src/config.rs
@@ -0,0 +1,43 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum FailureType {
+    Error,
+    Delay,
+    Timeout,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Failure {
+    pub path: String,
+    pub failure_type: FailureType,
+    pub frequency: f32,
+    pub delay: u64,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Configuration {
+    pub failures: Vec<Failure>,
+    pub listener_address: String,
+    pub proxy_address: String,
+}
+
+impl Configuration {
+    pub fn print(&self) {
+        for f in &self.failures {
+            info!("{:?}", f);
+        }
+    }
+}
+
+pub fn init() -> Configuration {
+    let mut config = config::Config::default();
+    config
+        .merge(config::File::with_name("Configuration"))
+        .unwrap();
+    let config = config.try_into::<Configuration>().unwrap();
+
+    info!("Configuration: {:?}", config);
+
+    config
+}
